06550233cfa3524093891e972fe86c7863c1a86e
mitch <robertsmj1138@gmail.com>
2022-09-17 13:53:43 -0400

    cleanup and remove unnecessary packages

--- src/components/HelloWorld.spec.js
+++ src/components/HelloWorld.spec.js
@@ -1,7 +1,18 @@
+import HelloWorld from '@/components/HelloWorld.vue';
 import { describe, expect, it } from 'vitest';
+import { mount } from '@vue/test-utils';
 
 describe('HelloWorld', () => {
-  it('does something', () => {
-    expect(true).toBeTruthy();
+  it('renders the msg prop', () => {
+    // Arrange
+    const msg = 'foo';
+
+    // Act
+    const wrapper = mount(HelloWorld, {
+      props: { msg },
+    });
+
+    // Assert
+    expect(wrapper.text()).toContain(msg);
   });
 });
